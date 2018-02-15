f3fe5689e0c37e9c950289e6f4989f6e7d2d1bd6
Anwar Sagar <anwar.sagar@student.tut.fi>
2018-02-15 22:59:44 +0200

    Start to handle the user input

--- Code/code1.cpp
+++ Code/code1.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	cout << "Hello World!" << endl;
+	cout << "Please type in you name?" << endl;
+	char userInput[50];
+	cin.getline(&userInput[0], 15);
+	cout << "You typed: " << userInput << endl;
 
 	return 0;
 }
