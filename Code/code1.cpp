ac2d6a869eff4afff87232f385903dae85d964ca
Anwar Sagar <anwar.sagar@student.tut.fi>
2018-02-19 13:06:18 +0200

    Added show_menu_options()

--- Code/code1.cpp
+++ Code/code1.cpp
@@ -2,17 +2,6 @@
 
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	cout << "Hello World!" << endl;
-	cout << "Please type in your name?" << endl;
-	char userInput[50];
-	cin.getline(&userInput[0], 15);
-	cout << "You typed: " << userInput << endl;
-
-	return 0;
-}
-
 bool do_string_contain_number(const string& strInput)
 {
 	bool retValue = false;
@@ -25,4 +14,23 @@ bool do_string_contain_number(const string& strInput)
 		// not implemented yet
 	}
 	return retValue;
+}
+
+void show_menu_options()
+{
+	string strMenu = "Welcome to try this code!\n";
+	strMenu.append("1 will close the application\n");
+	cout << strMenu.c_str() << endl;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello World!" << endl;
+	cout << "Please type in you name?" << endl;
+	char userInput[50];
+	cin.getline(&userInput[0], 15);
+	cout << "You typed: " << userInput << endl;
+	show_menu_options();
+
+	return 0;
 }
